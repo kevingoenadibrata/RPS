81d84a2fd4aac879e86f8c27e1e732dadee27101
kevin goenadibrata <kevingoenadibrata@gmail.com>
2019-08-16 05:17:31 +0000

    implemented room socket

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var socket = io.connect();
+
 function start(){
   anime({
     targets: '.far',
@@ -7,3 +9,41 @@ function start(){
     delay: anime.stagger(200, {easing: 'easeOutQuad'})
   });
 }
+
+
+
+$('#createbutton').on('click', () => {
+  var username = $('#username-c').val();
+  socket.emit('createRoom', {name: username});
+});
+
+$('#joinbutton').on('click', () => {
+  var username = $('#username-j').val();
+  var roomcode = $('#roomcode-j').val();
+  socket.emit('joinRoom', {name: username, roomCode: roomcode});
+});
+
+socket.on('newRoom', (data) => {
+  $('.container').hide();
+  $('.game-container').show();
+  $('#player1name').html(data.name);
+  $('#roomname').html(data.roomCode);
+});
+
+socket.on('codeError', () =>{
+  alert("Room Code doesn't exist");
+});
+
+
+socket.on('host', (data) =>{
+  alert(data.player2 + " joined the room");
+  $('#player2name').html(data.player2);
+});
+
+socket.on('guest', (data) =>{
+  $('.container').hide();
+  $('.game-container').show();
+  $('#player1name').html(data.player1);
+  $('#roomname').html(data.roomCode);
+  $('#player2name').html(data.player2);
+});
